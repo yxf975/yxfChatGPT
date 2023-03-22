edc9954db98ec2d0e1a6ed5015b17f19d163d4fe
Xuefeng <44554895+yxf975@users.noreply.github.com>
2023-03-22 16:33:44 +0800

    Update system.ts
    add new msg

--- src/system.ts
+++ src/system.ts
@@ -8,9 +8,9 @@ export const setting = {
 }
 
 export const message = `Powered by OpenAI Vercel
-- 由 [@ourongxing](https://github.com/ourongxing) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/ourongxing/chatgpt-vercel)，欢迎自部署。
-- 该网站仅作为演示，不提供长期服务，网站密码为 \`ourongxing\`，请勿滥用。
-- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
+- 由[@yxf975](https://github.com/yxf975) 基于[@ourongxing](https://github.com/ourongxing)和[chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/yxf975/yxfChatGPT)，欢迎自部署。
+- 该网站仅作为演示，不提供长期服务，网站暂时无密码，请勿滥用。
+- 本项目仅限亲朋好友使用，但不接受任何付费功能请求。
 - 该预览页面域名由 [@AUDI_GUZZ](https://m.okjike.com/users/4af3cfb4-1291-4a8b-b210-f515c86934a9) 和 [@Airyland](https://m.okjike.com/users/C6C8DE3A-E89D-4978-9E7D-B2E167D835A9) 免费提供，感谢。API Key 由我自己免费提供，注意不要滥用。
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
