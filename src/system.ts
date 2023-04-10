bfa8a85108368e502ea2117e203fb77eb10754c2
Xuefeng <44554895+yxf975@users.noreply.github.com>
2023-04-10 10:01:50 +0800

    Mod: msg

--- src/system.ts
+++ src/system.ts
@@ -8,7 +8,7 @@ export const setting = {
 }
 
 export const message = `Powered by OpenAI Vercel
-- 网站正在维护，如有需要请联系Xuefeng。
+- 网站正在维护（预计需要两三天时间），暂时不可用，如有需要请联系Xuefeng。
 - 由[@Xuefeng](https://github.com/yxf975) 基于[@ourongxing](https://github.com/ourongxing)和[chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/yxf975/yxfChatGPT)，欢迎自部署。
 - 该网站仅作为演示，仅限亲朋好友使用，不提供长期服务，且不接受任何付费功能请求，网站暂时无密码，请勿滥用。
 - 该网站域名由本人注册，请勿传播。API Key 由我自己免费提供，注意不要滥用。
